54ceaaea5e17cc9235d1918f4c7d4ffa30f13456
zhangyurui <zhangyurui@beisen.com>
2020-01-03 21:49:31 +0800

    feat: init

---
+++ src/dataSet.js
@@ -0,0 +1,14 @@
+export relyField = {
+  compareType: "eq",
+  value: ["1"],
+  displayContent: {
+    fields: [],
+    parts: ["Name", "Email" ,"9652bcfe-c6f7-4f9e-aa59-9d6af98e0b0c"]
+  },
+  hideContent: {
+    fields: [],
+    parts: ["9652bcfe-c6f7-4f9e-aa59-9d6af98e0b0c"]
+  },
+  requiredFields: ["Birthday"],
+  notRequiredFields: ["HomePhone"]
+}
---
+++ src/validate.js
@@ -0,0 +1,1 @@
+import includes from 'lodash/includes';
