5a0d4dd6572913bbcc4bf967bb8b5965f36c1b5f
zhangyurui <zhangyurui@beisen.com>
2020-01-09 08:52:28 +0800

    feat(outerule): 添加外层的校验规则，去掉一部分warning

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,7 @@
 import includes from 'lodash/includes';
-import { ds as valueMap } from './dataSet';
+// import { ds as valueMap } from './dataSet';
 
+import uniq from 'lodash/uniq';
 export const objToArr = (obj) => {
   if (typeof obj !== Object) return obj;
   let keys = Object.keys(obj);
@@ -31,19 +32,20 @@ export const dealRelyFieldData = (fieldData) => {
 
 export const contains = (arr1 = [], arr2 = []) => {
   if ((arr1 && !arr1.length) || (arr2 && !arr2.length) || arr1[0] === '') return false;
-  return arr1.some(item => includes(arr2, item))
+  let target = arr1.some(item => includes(arr2, item))
+  return target;
 }
 // 将显示隐藏只读Obj 转为数组
-const mapObjToArr = (obj) => {
-  let newObj = Object.assign({}, obj);
-  if (obj instanceof Object) {
-    let keys = Object.keys(newObj);
-    keys.forEach(key => newObj[key] = objToArr(obj[key]));
-  }
-  return newObj
-}
+// const mapObjToArr = (obj) => {
+//   let newObj = Object.assign({}, obj);
+//   if (obj instanceof Object) {
+//     let keys = Object.keys(newObj);
+//     keys.forEach(key => newObj[key] = objToArr(obj[key]));
+//   }
+//   return newObj
+// }
 // 同一规则不同条件的对比
-const compareSameRule = (condition1, condition2, index1, index2) => {
+const compareSameRule = (condition1, condition2, index1, index2, fieldVal) => {
   /*
    *  @params: condition: Object
    *           index:  Number
@@ -53,17 +55,38 @@ const compareSameRule = (condition1, condition2, index1, index2) => {
   condition2 = dealRelyFieldData(condition2);
   let standard = condition1.value && contains(condition1.value, condition2.value) && condition1.compareType === condition2.compareType
   if (standard) {
-    let { displayContent: displayContent1, hideContent: hideContent1, readOnly: readonlyContent1 } = condition1;
-    let { displayContent: displayContent2, hideContent: hideContent2, readOnly: readonlyContent2 } = condition2;
-    if (contains(displayContent1, hideContent2) || contains(displayContent2, hideContent1)) {
-      tipsArr.push(`条件${index1 + 1} 和 条件 ${index2 + 1}中显示(编辑)与隐藏重复\n`);
+    let { displayContent: displayContent1, hideContent: hideContent1, readOnly: readonlyContent1,
+      requiredFields: requiredFields1, notRequiredFields: notRequiredFields1
+    } = condition1;
+    let { displayContent: displayContent2, hideContent: hideContent2, readOnly: readonlyContent2,
+      requiredFields: requiredFields2, notRequiredFields: notRequiredFields2
+    } = condition2;
+    const matchTips = (fieldVal) => {
+      if (contains(displayContent1, hideContent2) || contains(displayContent2, hideContent1)) {
+        tipsArr.push(`${fieldVal}在条件${index1 + 1} 和 条件 ${index2 + 1}的显示(编辑)与隐藏中重复\n`);
+      }
+      if (contains(displayContent1, readonlyContent2) || contains(displayContent2, readonlyContent1)) {
+        tipsArr.push(`${fieldVal}在条件${index1 + 1} 和 条件 ${index2 + 1}的显示(编辑)与显示(只读)中重复\n`);
+      }
+      if (contains(readonlyContent1, hideContent2) || contains(readonlyContent2, hideContent1)) {
+        tipsArr.push(`${fieldVal}在条件${index1 + 1} 和 条件 ${index2 + 1}的显示(只读)与隐藏重复\n`);
+      }
+      if (contains(requiredFields1, notRequiredFields2) || contains(requiredFields2, notRequiredFields1)) {
+        tipsArr.push(`${fieldVal}在条件${index1 + 1} 和 条件 ${index2 + 1}的必填与非必填重复\n`);
+      }
     }
-    if (contains(displayContent1, readonlyContent2) || contains(displayContent2, readonlyContent1)) {
-      tipsArr.push(`条件${index1 + 1} 和 条件 ${index2 + 1}中显示(编辑)与显示(只读)重复\n`);
-    }
-    if (contains(readonlyContent1, hideContent2) || contains(readonlyContent2, hideContent1)) {
-      tipsArr.push(`条件${index1 + 1} 和 条件 ${index2 + 1}中显示(只读)与隐藏重复\n`);
-    }
+    let combineValue = [].concat(displayContent1,displayContent2,hideContent1,hideContent2,readonlyContent1,readonlyContent2);
+    combineValue = uniq(combineValue);
+    // combineValue.map(item => {
+    //   if(includes(value2, item)) return item;
+    // });
+    combineValue.forEach(v => {
+      const fieldLabelArr = fieldVal.filter(item => item.id === v) || [];
+      if (fieldLabelArr.length) fieldLabelArr.map(field => {
+        return matchTips(field.label);
+      })
+    });
+
   }
   return tipsArr;
 }
@@ -71,10 +94,10 @@ const concatAllData = (obj) => {
   let keys = Object.keys(obj);
   let concatResult = [];
   keys.map(key => {
-    if (obj[key] instanceof Object) {
-      if (key === 'value' || key === 'compareType') return;
-      obj[key] = mapObjToArr(obj[key]);
-      concatResult = concatResult.concat(obj[key]);
+    if (key === 'value' || key === 'compareType') return;
+    if (Array.isArray(obj[key])) {
+      // obj[key] = mapObjToArr(obj[key]);
+      if(obj[key].length) concatResult = concatResult.concat(obj[key]);
     }
     else concatResult.push(obj[key]);
   })
@@ -106,11 +129,14 @@ export const validateEachContent = (rules = {}, index = 0, aclField = '', fieldV
       tipsArr.push(`${fieldLabel}在条件${index + 1}的必填与非必填中中重复\n`);
     }
   }
+
+  let combineValue = [].concat(displayContent, hideContent, readOnly);
+  combineValue = uniq(combineValue);
   // 多个value逐条提示
-  value.forEach(v => {
-    const fieldLabelArr = fieldVal.filter(item => item.value === v) || [];
+  combineValue.forEach(v => {
+    const fieldLabelArr = fieldVal.filter(item => item.id === v) || [];
     if (fieldLabelArr.length) fieldLabelArr.map(field => {
-      matchTips(field.Name);
+      matchTips(field.label);
     })
   })
   return tipsArr;
@@ -129,11 +155,56 @@ export const matchSingleRule = (rules = [], aclField, fieldVal) => {
     if (blockTips.length) contentBlockTips = contentBlockTips.concat(blockTips);
     for (let j = i + 1; j < len; j++) {
       let rules2 = dealRelyFieldData(rules[j])
-      let tipArr = compareSameRule(rules2, rules2, i, j);
+      let tipArr = compareSameRule(rules1, rules2, i, j,fieldVal);
       if (tipArr.length) conditionBlockTips = conditionBlockTips.concat(tipArr);
     }
   }
   str = str.concat(contentBlockTips, conditionBlockTips);
   return str;
 }
-
+export const matchOuterRule = (data, fields) => {
+  let tips = '';
+  for(let i = 0, len = data.length; i < len ; i++) {
+    for(let j = i + 1; j < len; j++) {
+      const { aclField, aclType } = data[i];
+      if(aclField === data[j].aclField && aclType === data[j].aclType) {
+        let aclFieldLabel = fields.find(field => {
+          return field.id === aclField;
+        });
+        tips = validateOuterRule(data[i].relyField, data[j].relyField, aclFieldLabel.label);
+      }
+    }
+  }
+  if(tips.length) return tips;
+}
+const checkRepeatRule = (condition1, condition2, aclField) => {
+  condition1 = dealRelyFieldData(condition1);
+  condition2 = dealRelyFieldData(condition2);
+  let standard = condition1.value && contains(condition1.value, condition2.value) && condition1.compareType === condition2.compareType
+  if (standard) {
+    let { displayContent: displayContent1, hideContent: hideContent1, readOnly: readonlyContent1 } = condition1;
+    let { displayContent: displayContent2, hideContent: hideContent2, readOnly: readonlyContent2 } = condition2;
+     if (contains(displayContent1, hideContent2) || contains(displayContent2, hideContent1)) {
+       return true;
+    }
+    if (contains(displayContent1, readonlyContent2) || contains(displayContent2, readonlyContent1)) {
+      return true;
+    }
+    if (contains(readonlyContent1, hideContent2) || contains(readonlyContent2, hideContent1)) {
+      return true;
+    }
+  }
+  return false; 
+}
+export const validateOuterRule = (rule1, rule2, aclField) =>{
+  let tips = '';
+   for(let i = 0, len1 = rule1.length; i < len1 ; i++) {
+    for(let j = 0, len2 = rule2.length; j < len2; j++) {
+       if(checkRepeatRule(rule1[i], rule2[j])) {
+         tips = `${aclField}绑定的联动规则有重复`;
+         break;
+       }
+    }
+  }
+  return tips;
+}
